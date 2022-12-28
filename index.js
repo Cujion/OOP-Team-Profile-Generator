7b3dd4c979d43d03b435d476ea2c125abe21f87b
Chris <106836962+Cujion@users.noreply.github.com>
2022-12-28 16:46:25 -0500

    added fs require and prompt inquirer, added function for manager prompt questions

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const prompt =require('inquirer').prompt();
+const fs = require('fs');
+let team = [];
+
+
+function managerQuestions() {
+    return prompt([
+        {
+                name: 'name',
+                message: 'What is the managers name?',
+        },
+        {
+            name: 'id',
+            message: 'What is the managers id number?',
+        },
+        {
+            name: 'email',
+            message: 'What is the managers email?',
+        },
+        {
+            name: 'office',
+            message: 'What is the managers office number?',
+        },
+    ]).then(answers => {
+        let manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+        team.push(manager);
+        doNext();
+    })
+}
