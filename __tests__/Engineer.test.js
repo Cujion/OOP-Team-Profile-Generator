bac19ee6cb69e6c5f6004a7687f4df65070fa6f6
Chris <106836962+Cujion@users.noreply.github.com>
2022-12-28 16:55:14 -0500

    fixed code in wrong file and put it in correct file for engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Engineer", () => {
+test("Engineer github", () => {
+    const employee = new Engineer("John", 1, "somebody@gmail.com", "somebody");
+    expect(employee.github).toBe("somebody");
+});
 
+test("Engineer email", () => {
+    const employee = new Engineer("John", 1, "somebody@gmail.com");
+    expect(employee.email).toBe("somebody@gmail.com");
+});
+
+test("Engineer id", () => {
+    const employee = new Engineer("John", 1);
+    expect(employee.id).toBe(1);
+});
+
+test("Engineer name", () => {
+    const employee = new Engineer("John");
+    expect(employee.name).toBe("John");
 });
