637262b8c1bc3ca3d9b95bda3c01a57c768344d5
Chris <106836962+Cujion@users.noreply.github.com>
2022-12-28 16:56:17 -0500

    added tests for manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,22 @@
 const Manager = require("../lib/Manager");
 
-describe("Manager", () => {
+test("Manager office Number", () => {
+    const employee = new Manager("John", 1, "somebody@gmail.com", 2);
+    expect(employee.officeNumber).toBe(2);
+});
 
+test("Manager email", () => {
+    const employee = new Manager("John", 1, "somebody@gmail.com");
+    expect(employee.email).toBe("somebody@gmail.com");
+});
+
+test("Manager id", () => {
+    const employee = new Manager("John", 1);
+    expect(employee.id).toBe(1);
+});
+
+test("Manager name", () => {
+    const employee = new Manager("John");
+    expect(employee.name).toBe("John");
 });
 
