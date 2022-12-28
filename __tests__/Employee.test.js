2078aefec17afa4a185abe5561b5f8c8e9299c30
Chris <106836962+Cujion@users.noreply.github.com>
2022-12-28 16:52:31 -0500

    added tests for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,16 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
+test("Employee email", () => {
+    const employee = new Employee("John", 1, "somebody@gmail.com");
+    expect(employee.email).toBe("somebody@gmail.com");
+});
 
+test("Employee id", () => {
+    const employee = new Employee("John", 1);
+    expect(employee.id).toBe(1);
+});
+
+test("Employee name", () => {
+    const employee = new Employee("John");
+    expect(employee.name).toBe("John");
 });
