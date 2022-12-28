45ea2694593eb5da7e80fb0b30d9c1b0d134368a
Chris <106836962+Cujion@users.noreply.github.com>
2022-12-28 16:55:42 -0500

    added tests for intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,21 @@
 const Intern = require("../lib/Intern");
 
-describe("Intern", () => {
+test("Intern school", () => {
+    const employee = new Intern("John", 1, "somebody@gmail.com", "university");
+    expect(employee.school).toBe("university");
+});
 
+test("Intern email", () => {
+    const employee = new Intern("John", 1, "somebody@gmail.com");
+    expect(employee.email).toBe("somebody@gmail.com");
+});
+
+test("Intern id", () => {
+    const employee = new Intern("John", 1);
+    expect(employee.id).toBe(1);
+});
+
+test("Intern name", () => {
+    const employee = new Intern("John");
+    expect(employee.name).toBe("John");
 });
